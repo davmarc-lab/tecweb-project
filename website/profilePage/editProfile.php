8b972606ab2fdc84f1b9d0ac97723f32bcaca6f5
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2023-12-29 19:03:58 +0100

    [MOD] First implementation of form submit for editProfile.php, added the image to change the profile's icon

--- website/profilePage/editProfile.php
+++ website/profilePage/editProfile.php
@@ -10,56 +10,81 @@
 <body>
     <?php
     require_once("../includes/database.php");
-    $user = $dbh->execQuery("SELECT * FROM utente WHERE utente.IdUser=1")[0];
+    if (!isset($_SESSION["IdUser"])) {
+        // login not done
+        header("location:../login/login.php");
+    }
+    $userId = $_SESSION["IdUser"];
+    $user = $dbh->execQuery("SELECT * FROM utente WHERE utente.IdUser=$userId")[0];
+
+    // login already done
+    if (!isset($_POST["submit"])) {
     ?>
-    <div class="container-fluid overflow-hidden px-0">
-        <main>
-            <div class="row justify-content-between">
-                <section class="col-md-6 d-none d-md-block">
-                    <?php include_once("profilePage.php"); ?>
-                </section>
-                <section class="col-12 bg-light p-4">
-                    <a href="profilePage.php" role="button" class="btn btn-light d-md-none mb-3">
-                        <img src="../icons/backIcon2.png" class="p-0" alt="" />
-                    </a>
-                    <form>
-                        <div class="mb-2">
-                            <label for="inputName">Name:</label>
-                            <input type="text" id="inputName" class="form-control" value="<?php echo $user["Name"]; ?>" />
-                        </div>
-                        <div class="mb-2">
-                            <label for="inputSurname">Surname</label>
-                            <input type="text" id="inputSurname" class="form-control" value="<?php echo $user["Surname"]; ?>" />
-                        </div>
-                        <!--<div class="mb-2">
-                            <label for="inputDate">Birth date</label>
-                            <input type="date" id="inputDate" class="form-control" value="<?php echo $user["Date"]; ?>" />
-                        </div>-->
-                        <div class="mb-2">
-                            <label for="inputEmail">Email</label>
-                            <input type="email" id="inputEmail" class="form-control" value="<?php echo $user["Email"]; ?>" />
-                        </div>
-                        <div class="mb-2">
-                            <label for="inputPassword">Password</label>
-                            <input type="password" id="inputPassword" class="form-control" value="Password" />
-                        </div>
-                        <div class="mb-2">
-                            <label for="inputPassword">Repeat password</label>
-                            <input type="password" id="inputPassword" class="form-control" value="Repeat password" />
-                        </div>
-                        <div class="mb-2">
-                            <label for="inputDescription">Description *</label>
-                            <textarea id="inputDescription" class="form-control" rows="4" value="<?php echo $user["Description"]; ?>"></textarea>
-                        </div>
-                        <div class="d-flex">
-                            <button type="reset" class="btn btn-outline-danger ms-auto m-2">Cancel</button>
-                            <button type="submit" class="btn btn-outline-success m-2">Save</button>
-                        </div>
-                    </form>
-                </section>
-            </div>
-        </main>
-    </div>
+        <div class="container-fluid overflow-hidden px-0">
+            <main>
+                <div class="row justify-content-between">
+                    <section class="col-md-6 d-none d-md-block">
+                        <?php include_once("profilePage.php"); ?>
+                    </section>
+                    <section class="col-12 bg-light p-4">
+                        <a href="profilePage.php" role="button" class="btn btn-light d-md-none mb-3">
+                            <img src="../icons/backIcon2.png" class="p-0" alt="back" />
+                        </a>
+                        <a href="#" role="button" class="btn btn-light d-md-none mb-3">
+                            <img src="../icons/edit.png" class="p-0" alt="edit icon" />
+                        </a>
+                        <form action="<?php echo ($_SERVER["PHP_SELF"]); ?>" method="post" id="editProfileForm">
+                            <div class="mb-2">
+                                <label for="inputName" class="form-label">Name:</label>
+                                <input type="text" id="inputName" name="name" class="form-control" value="<?php echo $user["Name"]; ?>" />
+                            </div>
+                            <div class="mb-2">
+                                <label for="inputSurname" class="form-label">Surname</label>
+                                <input type="text" id="inputSurname" name="surname" class="form-control" value="<?php echo $user["Surname"]; ?>" />
+                            </div>
+                            <div class="mb-2">
+                                <label for="inputUsername" class="form-label">Username</label>
+                                <input type="text" id="inputUsername" name="username" class="form-control" value="<?php echo $user["Username"]; ?>" />
+                            </div>
+                            <div class="mb-2">
+                                <label for="inputEmail" class="form-label">Email</label>
+                                <input type="email" id="inputEmail" name="email" class="form-control" value="<?php echo $user["Email"]; ?>" />
+                            </div>
+                            <div class="mb-2">
+                                <label for="inputPassword" class="form-label">Password</label>
+                                <input type="password" id="inputPassword" name="password" class="form-control" value="Password" />
+                            </div>
+                            <div class="mb-2">
+                                <label for="inputPassword" class="form-label">Repeat password</label>
+                                <input type="password" id="inputPassword" name="repeatPassword" class="form-control" value="Repeat password" />
+                            </div>
+                            <div class="mb-2">
+                                <label for="inputDescription" class="form-label">Description *</label>
+                                <textarea id="inputDescription" name="description" class="form-control" rows="4" value="<?php echo $user["Description"]; ?>"></textarea>
+                            </div>
+                            <div class="d-flex">
+                                <button type="reset" class="btn btn-outline-danger ms-auto m-2">Cancel</button>
+                                <button type="submit" class="btn btn-outline-success m-2">Save</button>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+            </main>
+        </div>
+    <?php } else {
+        $name = $_POST["name"];
+        $surname = $_POST["surname"];
+        $username = $_POST["username"]
+        $email = $_POST["email"];
+        $password = $_POST["repeatPassword"]; //to modify!
+        $description = empty($_POST["description"]) ? "" : $_POST["description"];
+
+        $query = "UPDATE utente 
+            SET Name='$name', Surname='$surname', Username='$username', Email='$email', Password='$password', Description='$description' 
+            WHERE IdUser=$userId";
+        $res = $dbh->execQuery($query);
+        print_r($res);
+    } ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
