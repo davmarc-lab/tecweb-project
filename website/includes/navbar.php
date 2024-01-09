a00ed69a15066d15a6f6102c2f74daca3054d874
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2024-01-09 19:40:21 +0100

    [MOD] Aligned icons with links in the navbar

--- website/includes/navbar.php
+++ website/includes/navbar.php
@@ -36,32 +36,32 @@ class Navbar {
                     <div class="offcanvas-body">
                         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                             <li class="nav-item p-2">
-                                <a class="icon-link icon-link-hover link-dark link-underline-opacity-0 link-underline-opacity-75-hover" href="<?php echo(($this->homePath) . ($this->homePage)); ?>">
-                                    <i class="bi bi-house"></i>
+                                <a class="icon-link icon-link-hover link-dark link-underline-opacity-0 link-underline-opacity-75-hover"href="<?php echo(($this->homePath) . ($this->homePage)); ?>">
+                                    <i class="bi bi-house mb-2" aria-hidden="true"></i>
                                     Home
                                 </a>
                             </li>
                             <li class="nav-item p-2">
                                 <a class="icon-link icon-link-hover link-dark link-underline-opacity-0 link-underline-opacity-75-hover" href="<?php echo(($this->homePath) . ($this->searchPage)); ?>">
-                                    <i class="bi bi-search"></i>
+                                    <i class="bi bi-search mb-2"></i>
                                     Search
                                 </a>
                             </li>
                             <li class="nav-item p-2">
                                 <a class="icon-link icon-link-hover link-dark link-underline-opacity-0 link-underline-opacity-75-hover" href="<?php echo(($this->homePath) . ($this->notifPage)); ?>">
-                                    <i class="bi bi-bell"></i>
+                                    <i class="bi bi-bell mb-2"></i>
                                     Notification
                                 </a>
                             </li>
                             <li class="nav-item p-2">
                                 <a class="icon-link icon-link-hover link-dark link-underline-opacity-0 link-underline-opacity-75-hover" href="<?php echo(($this->homePath) . ($this->newPostPage)); ?>">
-                                    <i class="bi bi-plus-circle"></i>
+                                    <i class="bi bi-plus-circle mb-2"></i>
                                     New Post
                                 </a>
                             </li>
                             <li class="nav-item p-2">
                                 <a class="icon-link icon-link-hover link-dark link-underline-opacity-0 link-underline-opacity-75-hover" href="<?php echo(($this->homePath) . ($this->profilePage)); ?>">
-                                    <i class="bi bi-person-fill"></i>
+                                    <i class="bi bi-person-fill mb-2"></i>
                                     Profile
                                 </a>
                             </li>
