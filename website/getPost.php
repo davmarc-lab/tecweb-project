45352d05f4c95717824dd2ba194c8c69d4ed8467
penazziriccardo <penazziriccardo17@gmail.com>
2024-01-11 12:46:41 +0100

    [MOD] added more button for every post in home and modified comment button and like button to be better with dark theme

--- website/getPost.php
+++ website/getPost.php
@@ -19,10 +19,13 @@ foreach ($_SESSION['homePagePosts'] as $post) {
     $ret .= '<div class="card-body">';
     $ret .= '<div class="row">';
     $ret .= '<div class="col">';
-    $ret .= '<button type="button" class="btn btn-lg ' . (getClass($dbh, $post['IdPost']) ? "d-none" : "") . '" style="border: none; background: white;" onClick="likePost(' . $post['IdPost'] . ')" id="bttLike' . $post['IdPost'] . '"><i class="bi bi-hand-thumbs-up"></i></button>';
-    $ret .= '<button type="button" class="btn btn-lg ' . (getClass($dbh, $post['IdPost']) ? "" : "d-none") . '" style="border: none; background: white;" onClick="dislikePost(' . $post['IdPost'] . ')" id="bttLikeFill' . $post['IdPost'] . '"><i class="bi bi-hand-thumbs-up-fill"></i></button>';
+    $ret .= '<button type="button" class="btn btn-lg ' . (getClass($dbh, $post['IdPost']) ? "d-none" : "") . '" style="border: none;" onClick="likePost(' . $post['IdPost'] . ')" id="bttLike' . $post['IdPost'] . '"><i class="bi bi-hand-thumbs-up"></i></button>';
+    $ret .= '<button type="button" class="btn btn-lg ' . (getClass($dbh, $post['IdPost']) ? "" : "d-none") . '" style="border: none;" onClick="dislikePost(' . $post['IdPost'] . ')" id="bttLikeFill' . $post['IdPost'] . '"><i class="bi bi-hand-thumbs-up-fill"></i></button>';
     $ret .= '<span class="badge bg-secondary ms-4">' . $post['NumberComment'] . '</span>';
-    $ret .= '<button type="button" class="btn btn-lg" style="border: none; background: white;"><i class="bi bi-chat-left-text"></i></button>';
+    $ret .= '<button type="button" class="btn btn-lg" style="border: none;"><i class="bi bi-chat-left-text"></i></button>';
+    $ret .= '<a href="post/viewPost.php?id=' . $post["IdPost"] . '" class="float-end">';
+    $ret .= '<button class="btn btn-primary">More</button>';
+    $ret .= '</a>';                              
     $ret .= '<h5 class="card-title">' . $post["Title"] . '</h5>';
     $ret .= '<p class="card-text">' . $post["Description"] . '</p>';
     $ret .= '</div>';
