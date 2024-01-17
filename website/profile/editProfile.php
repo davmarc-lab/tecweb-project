32b243ddcafd8148e1f9774375e3eb3f108e9fd0
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2024-01-17 19:15:39 +0100

    [FIX] Fixed id names

--- website/profile/editProfile.php
+++ website/profile/editProfile.php
@@ -44,30 +44,30 @@
                             <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
                         </svg>
                     </a>
-                    <form action="<?php echo ($_SERVER["PHP_SELF"]); ?>" method="post" id="editProfileForm" enctype="multipart/form-data">
+                    <form action="<?php echo ($_SERVER["PHP_SELF"]); ?>" method="post" id="edit-profile-form" enctype="multipart/form-data">
                         <div class="mb-2">
-                            <label for="inputImage" class="form-label">Upload new icon:</label>
-                            <input type="file" name="files" id="inputImage" accept="image/*" class="form-control" />
+                            <label for="input-image" class="form-label">Upload new icon:</label>
+                            <input type="file" name="files" id="input-image" accept="image/*" class="form-control" />
                         </div>
                         <div class="mb-2">
-                            <label for="inputName" class="form-label">Name:</label>
-                            <input type="text" id="inputName" name="name" class="form-control" value="<?php echo $user["Name"]; ?>" />
+                            <label for="input-name" class="form-label">Name:</label>
+                            <input type="text" id="input-name" name="name" class="form-control" value="<?php echo $user["Name"]; ?>" />
                         </div>
                         <div class="mb-2">
-                            <label for="inputSurname" class="form-label">Surname</label>
-                            <input type="text" id="inputSurname" name="surname" class="form-control" value="<?php echo $user["Surname"]; ?>" />
+                            <label for="input-surname" class="form-label">Surname</label>
+                            <input type="text" id="input-surname" name="surname" class="form-control" value="<?php echo $user["Surname"]; ?>" />
                         </div>
                         <div class="mb-2">
-                            <label for="inputUsername" class="form-label">Username</label>
-                            <input type="text" id="inputUsername" name="username" class="form-control" value="<?php echo $user["Username"]; ?>" />
+                            <label for="input-username" class="form-label">Username</label>
+                            <input type="text" id="input-username" name="username" class="form-control" value="<?php echo $user["Username"]; ?>" />
                         </div>
                         <div class="mb-2">
-                            <label for="inputEmail" class="form-label">Email</label>
-                            <input type="email" id="inputEmail" name="email" class="form-control" value="<?php echo $user["Email"]; ?>" />
+                            <label for="input-email" class="form-label">Email</label>
+                            <input type="email" id="input-email" name="email" class="form-control" value="<?php echo $user["Email"]; ?>" />
                         </div>
                         <div class="mb-2">
-                            <label for="inputDescription" class="form-label">Description *</label>
-                            <textarea id="inputDescription" name="description" class="form-control" rows="4" form="editProfileForm"><?php echo $user["Description"]; ?></textarea>
+                            <label for="input-description" class="form-label">Description *</label>
+                            <textarea id="input-description" name="description" class="form-control" rows="4" form="editProfileForm"><?php echo $user["Description"]; ?></textarea>
                         </div>
                         <div class="d-flex">
                             <label for="reset" class="form-label" hidden>Cancel</label>
