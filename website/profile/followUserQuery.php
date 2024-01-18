3f148a4461a31d30161c6d6c2d8b7d5158190f2d
penazziriccardo <penazziriccardo17@gmail.com>
2024-01-18 19:12:55 +0100

    [MOD] modified like and follow

--- website/profile/followUserQuery.php
+++ website/profile/followUserQuery.php
@@ -5,21 +5,28 @@
 
     $srcUser = $_SESSION["userId"];
     $dstUser = $_POST['dstUser'];
-    $query = "INSERT INTO follow (IdSrc, IdDst) VALUES ($srcUser, $dstUser);";
-    $dbh->execQuery($query);
-    $targetId = mysqli_insert_id($dbh->getDataBaseController());
 
-    $query = "SELECT Username from member WHERE IdUser = {$srcUser};";
-    $username = $dbh->execQuery($query)[0]["Username"];
+    $query = "SELECT Id from follow WHERE IdSrc = $srcUser AND IdDst = $dstUser;";
+    $test = $dbh->execQuery($query);
 
-    $tmp = NotificationType::FOLLOWER->value;
-    $query = "INSERT INTO notification (Type, Description, IsRead, IdUser, IdTarget) VALUES ('$tmp', '{$username} started following you', 0, {$dstUser}, {$targetId});";
-    $dbh->execQuery($query);
-
-    $query = "UPDATE member SET NumberFollower = NumberFollower + 1 WHERE IdUser = '{$dstUser}';";
-    $dbh->execQuery($query);
+    if (count($test) == 0) {
+        $query = "INSERT INTO follow (IdSrc, IdDst) VALUES ($srcUser, $dstUser);";
+        $dbh->execQuery($query);
+        $targetId = mysqli_insert_id($dbh->getDataBaseController());
     
-    if (!checkUserOnline($dbh, $dstUser)) {
-        sendEmailNotification(getUserMail($dbh, $dstUser), "You have a new follower", "{$username} started following you");
+        $query = "SELECT Username from member WHERE IdUser = {$srcUser};";
+        $username = $dbh->execQuery($query)[0]["Username"];
+    
+        $tmp = NotificationType::FOLLOWER->value;
+        $query = "INSERT INTO notification (Type, Description, IsRead, IdUser, IdTarget) VALUES ('$tmp', '{$username} started following you', 0, {$dstUser}, {$targetId});";
+        $dbh->execQuery($query);
+    
+        $query = "UPDATE member SET NumberFollower = NumberFollower + 1 WHERE IdUser = '{$dstUser}';";
+        $dbh->execQuery($query);
+        
+        if (!checkUserOnline($dbh, $dstUser)) {
+            sendEmailNotification(getUserMail($dbh, $dstUser), "You have a new follower", "{$username} started following you");
+        }
     }
+
 ?>
--- website/profile/unfollowUserQuery.php
+++ website/profile/unfollowUserQuery.php
@@ -6,17 +6,22 @@ include_once("../includes/utils.php");
 $srcUser = $_SESSION["userId"];
 $dstUser = $_POST['dstUser'];
 
+
+
 // find correct follow id
 $query = "SELECT * FROM follow WHERE IdSrc = {$srcUser} AND IdDst = {$dstUser};";
-$followId = $dbh->execQuery($query)[0]["Id"];
-
-// remove follow
-$query = "DELETE FROM follow WHERE Id = $followId";
-$dbh->execQuery($query);
-
-$tmp = NotificationType::FOLLOWER->value;
-$query = "DELETE from notification WHERE Type = '{$tmp}' AND IdUser = {$dstUser} AND IdTarget = {$followId};";
-$dbh->execQuery($query);
-
-$query = "UPDATE member SET NumberFollower = NumberFollower - 1 WHERE IdUser = '{$dstUser}';";
-$dbh->execQuery($query);
+$test = $dbh->execQuery($query);
+if (count($test) > 0) {
+    $followId = $test[0]["Id"];
+    
+    // remove follow
+    $query = "DELETE FROM follow WHERE Id = $followId";
+    $dbh->execQuery($query);
+    
+    $tmp = NotificationType::FOLLOWER->value;
+    $query = "DELETE from notification WHERE Type = '{$tmp}' AND IdUser = {$dstUser} AND IdTarget = {$followId};";
+    $dbh->execQuery($query);
+    
+    $query = "UPDATE member SET NumberFollower = NumberFollower - 1 WHERE IdUser = '{$dstUser}';";
+    $dbh->execQuery($query);
+}
