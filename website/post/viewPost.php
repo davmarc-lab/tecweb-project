e5455cd265ca0b4a88ff8ab5b634b90b3da3393d
penazziriccardo <penazziriccardo17@gmail.com>
2024-01-18 15:44:52 +0100

    [FIX] fixed btn go back in viewPost

--- website/post/viewPost.php
+++ website/post/viewPost.php
@@ -40,7 +40,7 @@ $infoPost = $dbh->execQuery($query)[0];
     <div class="container-fluid col-12 col-md-8 mx-auto py-auto py-3 d-block">
         <div id="view-post" class="border rounded p-3">
             <!-- post section -->
-            <a href="javascript: history.go(-1)" role="button" class="btn btn-utility mb-3" title="Go back">
+            <a href="javascript: history.go(-1)" role="button" class="btn btn-utility-contrast mb-3" title="Go back">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
                 </svg>
