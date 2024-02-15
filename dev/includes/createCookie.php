a180f0e7ec64904b5d67c2e0a4f2d570ad0863ba
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2024-02-15 19:15:52 +0100

    prepared mvc standard

--- dev/includes/createCookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_COOKIE["user" . $_SESSION["userId"]])) {
-    $cookieName = "user" . $_SESSION["userId"];
-    $cookieValue = $_SESSION["userId"];
-    setcookie($cookieName, $cookieValue, strtotime( '+30 days' ), "/");
-    echo $_COOKIE[$cookieName];
-}
-?>
--- dev/post/createCategory.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("../includes/database.php");
-    $categoryDescription = $_POST["description"];
-    $query = "INSERT INTO category (Description) VALUES(\"$categoryDescription\");";
-    $dbh->execQuery($query);
-    $lastId = $dbh->getDataBaseController()->insert_id;
-    // echo for response value
-    echo($lastId);
-?>
