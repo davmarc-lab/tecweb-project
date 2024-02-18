fd757f0cb6d35f1c4d9c79f521373d69aac6986d
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2024-02-18 13:42:08 +0100

    [MOD] First implementation of css inside search page: 3 posts for each row with same size

--- website/search/search.php
+++ website/search/search.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" />
-    <link rel="stylesheet" href="../includes/style.css" />
+    <link rel="stylesheet" href="../includes/newStyle.css" />
     <link rel="stylesheet" href="../includes/scrollableMenu.css" />
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="searchScript.js" defer></script>
@@ -29,72 +28,66 @@
     $bar = new Navbar("../");
     $bar->drawNavbar("Search");
     ?>
-    <div class="container-fluid">
-        <div class="row mt-5">
-            <div class="input-group">
-                <label for="search-text" class="visually-hidden">Search</label>
-                <input type="text" id="search-text" name="search-text" class="form-control" placeholder="Search" aria-describedby="search-button" />
-                <button class="btn btn-outline-secondary btn-search" id="search-button" name="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                    </svg>
-                </button>
-            </div>
-            <div class="row mt-4">
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="category-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Choose categories
-                    </button>
-                    <div class="dropdown-menu p-1" aria-labelledby="category-dropdown">
-                        <div class="container d-flex">
-                            <label for="category-search" hidden>Choose zero or one or many categories</label>
-                            <input type="text" name="categorySearch" id="category-search" class="form-control" placeholder="Search category" />
-                            <a class="btn btn-utility-contrast btn-secondary ms-1" role="button" id="reset">Clear</a>
-                        </div>
-                        <div class="container mt-2 scrollable-menu">
-                            <?php
-                            $categories = getAllCategories($dbh);
-                            foreach ($categories as $cat) {
-                            ?>
-                                <a class="dropdown-item btn-primary rounded my-1" role="button" id="cat-<?php echo ($cat["IdCategory"]); ?>"><?php echo ($cat["Description"]); ?></a>
-                            <?php
-                            }
-                            ?>
-                        </div>
-                    </div>
-                    <div id="selected-categories"></div>
-                </div>
-                <div id="category-badges"></div>
-            </div>
+    <div class="cascading-container">
+        <div class="input-group">
+            <label for="search-text" class="visually-hidden" hidden>Search</label>
+            <input type="text" id="search-text" name="search-text" class="form-control" placeholder="Search" aria-describedby="search-button" />
+            <button class="btn btn-search" id="search-button" name="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+            </button>
         </div>
-        <?php
-        if (!isset($_SESSION["search_result"]["post"])) {
-        ?>
-            <div class="container-fluid justify-content-center align-items-center text-center">
-                <?php
-                $query = "SELECT * FROM post ORDER BY RAND() LIMIT 9;";
-                $res = $dbh->execQuery($query);
-                printPost($res, $dbh);
-                ?>
-            </div>
-        <?php
-        } else {
-        ?>
-            <div class="container-fluid">
-                <div class="row mt-4 justify-content-center">
-                    <div class="container col-md-8">
+        <div class="row mt-4">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="category-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Choose categories
+                </button>
+                <div class="dropdown-menu p-1" aria-labelledby="category-dropdown">
+                    <div class="container d-flex">
+                        <label for="category-search" hidden>Choose zero or one or many categories</label>
+                        <input type="text" name="categorySearch" id="category-search" class="form-control" placeholder="Search category" />
+                        <a class="btn btn-utility-contrast btn-secondary ms-1" role="button" id="reset">Clear</a>
+                    </div>
+                    <div class="container mt-2 scrollable-menu">
                         <?php
-                        if (!empty($_SESSION["search_result"]["profile"])) {
-                            printProfile($dbh, $_SESSION["search_result"]["profile"]);
+                        $categories = getAllCategories($dbh);
+                        foreach ($categories as $cat) {
+                        ?>
+                            <a class="dropdown-item btn-primary rounded my-1" role="button" id="cat-<?php echo ($cat["IdCategory"]); ?>"><?php echo ($cat["Description"]); ?></a>
+                        <?php
                         }
                         ?>
                     </div>
                 </div>
-                <div class="row justify-content-center align-items-center text-center">
+                <div id="selected-categories"></div>
+            </div>
+            <div id="category-badges"></div>
+        </div>
+        <?php
+        if (!isset($_SESSION["search_result"]["post"])) {
+        ?>
+            <?php
+            $query = "SELECT * FROM post ORDER BY RAND() LIMIT 9;";
+            $res = $dbh->execQuery($query);
+            printPost($res, $dbh);
+            ?>
+        <?php
+        } else {
+        ?>
+            <div class="row mt-4 justify-content-center">
+                <div class="container col-md-8">
                     <?php
-                    printPost($_SESSION["search_result"]["post"], $dbh);
+                    if (!empty($_SESSION["search_result"]["profile"])) {
+                        printProfile($dbh, $_SESSION["search_result"]["profile"]);
+                    }
                     ?>
                 </div>
+            </div>
+            <div class="card-row">
+                <?php
+                printPost($_SESSION["search_result"]["post"], $dbh);
+                ?>
             </div>
         <?php
         }
@@ -122,40 +115,39 @@ function printPost($res, $dbh)
         }
 
         if ($index == 0) {
-            echo "<div class=\"row mt-5 d-flex justify-content-center align-items-center text-center\">";
+            echo "<div class=\"card-row\">";
         }
         $query = "SELECT Username from member WHERE IdUser = '{$post["IdUser"]}';";
         $username = $dbh->execQuery($query)[0]["Username"];
 ?>
-        <div class="col-md-4 col-12 mx-auto my-5 d-flex justify-content-center">
-            <div class="card">
-                <p class="m-1"><?php echo drawLinkUsername($username, $post["IdUser"], "../profile/profilePage.php") ?></p>
+        <div class="card">
+            <p class="m-1"><?php echo drawLinkUsername($username, $post["IdUser"], "../profile/profilePage.php") ?></p>
+            <?php
+            if (!$empty) {
+            ?>
+                <img src="<?php echo $previewPath ?>" class="card-img-top" alt="Post preview">
+            <?php
+            }
+            $category = getCategory($dbh, $post);
+            ?>
+            <div class="card-body">
+                <p class="show-date"><?php echo substr($post["Date"], 0, 10); ?></p>
                 <?php
-                if (!$empty) {
+                if ($category !== NULL) {
                 ?>
-                    <img src="<?php echo $previewPath ?>" class="card-img-top p-1" alt="Post preview">
+                    <span class="badge border rounded-pill mb-2">
+                        <?php echo $category ?>
+                    </span>
                 <?php
                 }
-                $category = getCategory($dbh, $post);
                 ?>
-                <div class="card-body">
-                    <p class="show-date"><?php echo substr($post["Date"], 0, 10); ?></p>
-                    <?php
-                    if ($category !== NULL) {
-                    ?>
-                        <span class="badge border rounded-pill mb-2">
-                            <?php echo $category ?>
-                        </span>
-                    <?php
-                    }
-                    ?>
-                    <h5 class="card-title mt-2"><?php echo $post['Title']; ?></h5>
-                    <p class="card-text"><?php echo substr($post['Description'], 0, 100); ?>
-                        <?php echo strlen($post['Description']) > 100 ? '...' : ''; ?></p>
-                    <a href="../post/viewPost.php?id=<?php echo ($post["IdPost"]); ?>" role="button" class="btn btn-primary view-post">
-                        View post
-                    </a>
-                </div>
+                <h5 class="card-title"><?php echo $post['Title']; ?></h5>
+                <p class="card-text"><?php echo substr($post['Description'], 0, 100); ?>
+                    <?php echo strlen($post['Description']) > 100 ? '...' : ''; ?>
+                </p>
+                <a href="../post/viewPost.php?id=<?php echo ($post["IdPost"]); ?>" role="button" class="btn btn-primary view-post">
+                    View post
+                </a>
             </div>
         </div>
 <?php
