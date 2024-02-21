d0c09e5f56231748e9ae63d70eee8965f0bab0ef
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2024-02-21 18:39:23 +0100

    [FIX] first working version of send/receive messages

--- dev/controller/message/message.js
+++ dev/controller/message/message.js
@@ -24,6 +24,48 @@ function printAllMessages(div, messages, dst) {
         pMsg.innerHTML = elem['Content'];
         div.appendChild(pMsg);
     });
+
+    // add text box at the end of the div
+    let divUserInput = document.createElement('div');
+
+    let areaMessage = document.createElement('textarea');
+    areaMessage.placeholder = "Write your message here...";
+    divUserInput.appendChild(areaMessage);
+    
+    let sendMessage = document.createElement('button');
+    sendMessage.innerHTML = "Send";
+    sendMessage.disabled = areaMessage.value == "";
+    divUserInput.appendChild(sendMessage);
+
+    // event listeners
+    areaMessage.addEventListener('input', function () {
+        sendMessage.disabled = this.value == "";
+    });
+
+    sendMessage.addEventListener('click', function () {
+        // insert into database the new message
+        let messageText = areaMessage.value;
+
+        $.ajax({
+            url: "../model/message/sendMessage.php",
+            method: "POST",
+            data: {
+                message: messageText,
+                dst: dst,
+            },
+            success: function (response) {
+                let pNewMsg = document.createElement('p');
+                pNewMsg.innerHTML = messageText;
+                div.insertBefore(pNewMsg, divUserInput);
+
+                // clear text area
+                areaMessage.value = "";
+                sendMessage.disabled = true;
+            }
+        });
+    });
+
+    div.appendChild(divUserInput);
 }
 
 function loadMessages(userId) {
---
+++ dev/model/message/sendMessage.php
@@ -0,0 +1,16 @@
+<?php
+
+if (session_status() != PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+require_once "../../includes/database.php";
+
+$message = $_POST['message'];
+$dst = $_POST['dst'];
+$src = $_SESSION['userId'];
+
+$query = "INSERT INTO message (Content, IdSrc, IdDst)
+        VALUES ('$message', $src, $dst);";
+
+$dbh->execQuery($query);
