4b02b9325139ee11cc97d795282a187ff9229148
penazziriccardo <penazziriccardo17@gmail.com>
2024-02-29 19:32:51 +0100

    [MOD] fixed new post and added a new version of db

--- dev/controller/newPost/createNewPost.js
+++ dev/controller/newPost/createNewPost.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
         let formData = new FormData(document.getElementById("new-post-form"));
         const values = [...formData.entries()];
         console.log(values);
-        let category = document.getElementById("selected-badge").innerHTML;
+        let category;
+        if (document.getElementById("selected-badge") != null) {
+            category = document.getElementById("selected-badge").innerHTML;
+        }
         $.ajax({
             async: false,
             url: '../model/newPost/newPost.php',
