85ef89490b365af7561b03ea9b2351ae2acc2d03
penazziriccardo <penazziriccardo17@gmail.com>
2024-02-29 23:04:48 +0100

    [FIX] fixed random posts in search

--- dev/model/search/getRandomPosts.php
+++ dev/model/search/getRandomPosts.php
@@ -1,6 +1,10 @@
 <?php
+    if (session_status() != PHP_SESSION_ACTIVE) {
+        session_start();
+    }
     include_once("../../includes/database.php");
-    $query = "SELECT * FROM post WHERE Private = FALSE ORDER BY RAND() LIMIT 9;";
+
+    $query = "SELECT * FROM post WHERE Private = FALSE AND IdUser <> {$_SESSION['userId']} ORDER BY RAND() LIMIT 9;";
     $res = $dbh->execQuery($query);
     echo json_encode($res);
 ?>
