796a7e76c474a750c36875862d1f83cf9a5a4fd5
penazziriccardo <penazziriccardo17@gmail.com>
2024-03-01 13:27:50 +0100

    [FIX] fixed private more info for mobile users

--- dev/controller/newPost/newPost.js
+++ dev/controller/newPost/newPost.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     $('#navbar-space').load('../view/navbar.html');
+
+    document.getElementById("p-explain-private").style.display = "none";
+
+    document.getElementById("private-more-info").addEventListener("click", function () {
+        let pExplain = document.getElementById("p-explain-private");
+        let pStyle = window.getComputedStyle(pExplain);
+        if (pStyle.getPropertyValue("display") === "none") {
+            pExplain.style.display = "";
+        } else {
+            pExplain.style.display = "none";
+        }
+    });
 });
